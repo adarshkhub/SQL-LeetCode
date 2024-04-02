19a98478a3a7f5f73531198677099340cb1cdf3f
adarshkhub <67844710+adarshkhub@users.noreply.github.com>
2024-04-02 16:37:42 -0400

    Add files via upload

--- LeetCode SQL Select Statements.sql
+++ LeetCode SQL Select Statements.sql
@@ -1,3 +1,22 @@
 -- Write your PostgreSQL query statement below
+
+-- Q1
 select product_id from products
 where low_fats = 'Y' and recyclable = 'Y'
+
+--Q2
+select name from Customer
+where referee_id != 2 or referee_id is null;
+
+
+--Q3
+select name, population, area from World 
+where area >= 3000000 or population >= 25000000
+
+--Q4
+select distinct author_id as id from Views
+where author_id = viewer_id
+
+--Q5
+select tweet_id from Tweets
+where length(Content) > 15
