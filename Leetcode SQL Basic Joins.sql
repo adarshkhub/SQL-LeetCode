1353f0a4cf7fdf876d55d853c790d7800d6e4d0e
adarshkhub <67844710+adarshkhub@users.noreply.github.com>
2024-04-11 18:36:56 -0400

    Add files via upload

--- Leetcode SQL Basic Joins.sql
+++ Leetcode SQL Basic Joins.sql
@@ -39,9 +39,13 @@ where bonus < 1000 or bonus IS NULL
 -- 1 step is to join
 
 -- 1 step is to groupby and count the subject names per student_id
-
-select student_id, count(subject_name) as attended_exams
-
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.student_id) AS attended_exams
+FROM Students s
+CROSS JOIN Subjects sub
+LEFT JOIN Examinations e
+ON s.student_id = e.student_id AND sub.subject_name = e.subject_name
+GROUP BY s.student_id, s.student_name, sub.subject_name 
+ORDER BY s.student_id, sub.subject_name;
 
 
 
